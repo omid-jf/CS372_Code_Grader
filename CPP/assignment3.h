fee1d67808caaff07a4b6b76825babf6bd2077ad
Omid <omid.jafari@hotmail.com>
2022-10-03 18:16:44 -0600

    Added assignment 3

---
+++ CPP/assignment3.h
@@ -0,0 +1,21 @@
+//  ============================================================
+//  This file is part of the CS372_Code_Grader project.
+//
+//  Author: Omid Jafari - omidjafari.com
+//  Copyright (c) 2022
+//
+//  For the full copyright and license information, please view
+//  the LICENSE file that was distributed with this source code.
+//  ============================================================
+
+#ifndef CPP_ASSIGNMENT3_H
+#define CPP_ASSIGNMENT3_H
+
+#include <vector>
+#include <string>
+
+std::string question1(const std::string&, int&);
+std::string question2(const std::string&, int&);
+int question3(const std::vector<std::vector<int>>&);
+
+#endif //CPP_ASSIGNMENT3_H
